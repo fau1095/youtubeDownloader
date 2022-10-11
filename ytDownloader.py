a12f81d0de74987c6213039bb087f238a9c595d3
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2022-10-11 14:43:19 -0600

    Create ytDownloader.py

---
+++ ytDownloader.py
@@ -0,0 +1,31 @@
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+from pytube import YouTube
+from sys import argv
+from pathlib import Path
+import os
+import sys
+
+link = argv[1]
+yt = YouTube(link)
+folder = '/Users/fabriziomendez/Documents/Studio One/Multitracks/audiodownloader'
+
+print ("Title: ", yt.title)
+print ("Views: ", yt.views)
+
+# yd = yt.streams.get_highest_resolution()
+
+# yd.download("/Users/fabriziomendez/Downloads")
+
+ya = yt.streams.filter(only_audio=True)
+
+ya[1].download(folder)
+
+def downloadedFile():
+# used to rename file extension
+    for filename in os.listdir(folder):
+        infilename = os.path.join(folder,filename)
+        if not os.path.isfile(infilename): continue
+        oldbase = os.path.splitext(filename)
+        newname = infilename.replace('.mp4', '.mp3')
+        output = os.rename(infilename, newname)
+downloadedFile()
