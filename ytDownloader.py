d0d3e8201da4b7c7f608d152d04a7ff6878a5efc
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2023-07-31 21:36:56 -0600

    fixed the corruption issues

--- ytDownloader.py
+++ ytDownloader.py
@@ -27,20 +27,29 @@ def downloadedFile():
 # used to rename file extension
 # Convert mp4 to mp3
     file_name = yt.title + '.mp4'
+# Specify the complete file path for the input MP4 file
+    input_file_path = os.path.join(os.getcwd(),file_name)
     sound = AudioSegment.from_file(file_name, format="mp4")
 
 # Set the mp3 file name
-    mp3_file = yt.title + '.mp3'
-
+# Specify the complete file path for the output MP3 file
+    #mp3_file = yt.title + '.mp3'
+    mp3_file = os.path.join(os.getcwd(), yt.title + '.mp3')
 # Export the mp3 file
-    sound.export(mp3_file, format="mp3")
+    try:    
+        sound.export(mp3_file, format="mp3")
+    except Exception as e:
+        print("Error during MP4 to MP3 conversion:",e)    
 
 # Delete the mp4 file
-    os.remove(file_name)
+    #os.remove(file_name)
+    os.remove(input_file_path)
 
+# Create the destination folder if it doesn't exist    
     folder_location = myDesiredFolder
     if not os.path.exists(folder_location):
         os.makedirs(folder_location)
+# Move the MP3 file to the destination folder
     shutil.move(mp3_file, folder_location)
 
 downloadedFile()
